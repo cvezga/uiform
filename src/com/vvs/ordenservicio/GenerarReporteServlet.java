5c85276b794ccdc4d3eda2fa1f15d5c4591c2054
crva <cesar.vezga@gft.com>
2016-09-23 09:52:11 -0600

    new report and logo

--- src/com/vvs/ordenservicio/GenerarReporteServlet.java
+++ src/com/vvs/ordenservicio/GenerarReporteServlet.java
@@ -1,10 +1,12 @@
 package com.vvs.ordenservicio;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -49,7 +51,21 @@ public class GenerarReporteServlet extends HttpServlet {
 		parametrosReporte.put("ORDEN_SERVICIO", id);
 		Connection connection = DB.getConnection();
 		parametrosReporte.put("REPORT_CONNECTION", connection);
-		
+		URI uri= null;
+		try {
+			uri = new URI(request.getRequestURL().toString());
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+        String proto = "http://";
+        String host = uri.getHost();
+        String base = "/OrdenServicioView";
+        int port = uri.getPort();
+        String rutaLogo = proto+host+":"+port+base+ "/images/logo.png";
+        parametrosReporte.put("LOGO",rutaLogo);
+        parametrosReporte.put("NOMBRE_LOGO","Movistar");
+        parametrosReporte.put("CONTEXTO",request.getServletContext());
+	
 		response.setContentType("application/pdf");
 		response.setHeader("Content-disposition","inline; filename='OrdedServicio-"+id+".pdf'");
 		
