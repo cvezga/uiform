c3b4319f10515f9033bcc824e88eeb9ce6392128
Cesar Vezga <vcesar@yahoo.com>
2016-09-25 10:57:52 -0600

    added dn pool

--- src/com/vvs/ordenservicio/DB.java
+++ src/com/vvs/ordenservicio/DB.java
@@ -6,40 +6,56 @@ import java.sql.SQLException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class DB {
 
-	private static String host = "190.7.214.194";
-	private static String port = "14432";
-	private static String user = "postgres";
-	private static String pass = "pos_GREEN_BOX_2016";
-	private static String sche = "telefonica_cliente";
+	//private static String host = "190.7.214.194";
+	//private static String port = "14432";
+	//private static String user = "postgres";
+	//private static String pass = "pos_GREEN_BOX_2016";
+	//private static String sche = "telefonica_cliente";
 
-	/**
-	public void DS() {
-		InitialContext cxt = new InitialContext();
-		if (cxt == null) {
-			throw new Exception("Uh oh -- no context!");
+	 
+	public static Connection  getDSConnection() throws Exception  {
+		Connection conn = null;
+		InitialContext cxt;
+		try {
+			cxt = new InitialContext();
+			if (cxt == null) {
+				throw new Exception("Uh oh -- no context!");
+			}
+
+			DataSource ds = (DataSource) cxt.lookup("java:/comp/env/jdbc/postgres");
+
+			if (ds == null) {
+				throw new Exception("Data source not found!");
+
+			}else{
+				// ---------------
+
+				//Context initContext = new InitialContext();
+
+
+				//Context envContext = (Context) initContext.lookup("java:/comp/env");
+				//DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
+				conn = ds.getConnection();
+				// etc.
+			}
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new Exception(e);
 		}
-
-		DataSource ds = (DataSource) cxt.lookup("java:/comp/env/jdbc/postgres");
-
-		if (ds == null) {
-			throw new Exception("Data source not found!");
-
-			// ---------------
-
-			Context initContext = new InitialContext();
-			Context envContext = (Context) initContext.lookup("java:/comp/env");
-			DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
-			Connection conn = ds.getConnection();
-			// etc.
-		}
+		
+		return conn;
+		
 	}
-	**/
+	 
+	/**
 
-	public static Connection getConnection() {
+	public static Connection getConnectionOLD() {
 		Connection connection = null;
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -57,6 +73,18 @@ public class DB {
 		}
 
 		return connection;
+	}**/
+	
+	public static Connection getConnection() {
+		
+		try {
+			return getDSConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 
 	public static ConnectionHelper getConnectionHelper() {
